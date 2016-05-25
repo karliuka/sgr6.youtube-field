3b7c472df40050884f9e475bdb9b385a51d86254
alfu <wit.kharkov@gmail.com>
2016-05-25 11:57:40 +0300

    language files

---
+++ modules/DynamicFields/Ext/Language/uk_UA.youtube.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Faonni
+ *  
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade module to newer
+ * versions in the future.
+ * 
+ * @package     Field_Youtube
+ * @copyright   Copyright (c) 2016 Karliuka Vitalii(karliuka.vitalii@gmail.com) 
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+$mod_strings['LBL_YOUTUBE_WIDTH'] = 'Ширина';
+$mod_strings['LBL_YOUTUBE_HEIGHT'] = 'Висота';
+$mod_strings['LBL_YOUTUBE_LIST_WIDTH'] = 'Ширина у списку';
+$mod_strings['LBL_YOUTUBE_LIST_HEIGHT'] = 'Висота у списку';
